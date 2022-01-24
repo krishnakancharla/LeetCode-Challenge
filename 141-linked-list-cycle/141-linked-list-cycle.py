31538f15016ae3f321c27a764313e030a2ca9888
Krishnaditya Kancharla <36543617+krishnakancharla@users.noreply.github.com>
2022-01-23 16:23:50 -0800

    Time: 93 ms (18.01%), Space: 17.6 MB (57.30%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -14,8 +14,10 @@ class Solution:
         while slow!=fast:
             if fast is None or fast.next is None:
                 return False
-            slow = slow.next
-            fast= fast.next.next
+            slow=slow.next
+            fast=fast.next.next
             
-        return True    
-            
+        
+        return True
+        
+             
